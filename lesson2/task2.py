c6a4b762e677aecc836f6635511ab78bb18465ce
Daniar <daniar.ormanov1989@yandex.ru>
2021-02-03 22:22:44 +0300

    lesson2

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -6,21 +6,17 @@ before_mix = list()
 after_mix = list()
 number = input('Введите количество элементов в списке - ')
 if type(number) is int and int(number) > 0:
-    pass
+    i = 0
+    while i < int(number):
+        element = input('Введите элемент списка - ')
+        before_mix.insert(i, element)
+        if i % 2 == 0 and i != int(number)-1:
+            after_mix.insert(i + 1, element)
+        elif i % 2 != 0:
+            after_mix.insert(i - 1, element)
+        else:
+            after_mix.insert(i, element)
+        i += 1
+    print(before_mix, after_mix)
 else:
-    print('Введите количество элементов в списке - ')
-
-
-i = 0
-while i < int(number):
-    element = input('Введите элемент списка - ')
-    before_mix.insert(i, element)
-    if i % 2 == 0 and i != int(number)-1:
-        after_mix.insert(i + 1, element)
-    elif i % 2 != 0:
-        after_mix.insert(i - 1, element)
-    else:
-        after_mix.insert(i, element)
-    i += 1
-
-print(before_mix, after_mix)
+    print('Введите еще раз - ')
