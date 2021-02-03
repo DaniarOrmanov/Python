1aab564cc32a9c0442687edd5c41e78bfa7a59c7
Daniar <daniar.ormanov1989@yandex.ru>
2021-02-03 22:35:34 +0300

    lesson2

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -5,18 +5,24 @@
 before_mix = list()
 after_mix = list()
 number = input('Введите количество элементов в списке - ')
-if type(number) is int and int(number) > 0:
-    i = 0
-    while i < int(number):
-        element = input('Введите элемент списка - ')
-        before_mix.insert(i, element)
-        if i % 2 == 0 and i != int(number)-1:
-            after_mix.insert(i + 1, element)
-        elif i % 2 != 0:
-            after_mix.insert(i - 1, element)
-        else:
-            after_mix.insert(i, element)
-        i += 1
-    print(before_mix, after_mix)
-else:
-    print('Введите еще раз - ')
+flag = 0
+while flag == 0:
+    if int(number) > 0:
+        i = 0
+        while i < int(number):
+            element = input('Введите элемент списка - ')
+            before_mix.insert(i, element)
+            if i % 2 == 0 and i != int(number)-1:
+                after_mix.insert(i + 1, element)
+            elif i % 2 != 0:
+                after_mix.insert(i - 1, element)
+            else:
+                after_mix.insert(i, element)
+            i += 1
+        print(before_mix, after_mix)
+        flag = 1
+    else:
+        number = input('Надо ввести натуральное число - ')
+# Сделал защиту number от ввода не нанурального числа,
+# но защиту от ввода текста не сделал
+# Все элементы списка - это строки
