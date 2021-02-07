a1f183cecf0c6cd452e6f434473e23226c42cd63
Daniar <daniar.ormanov1989@yandex.ru>
2021-02-07 14:55:59 +0300

    lesson3

--- lesson3/task6.py
+++ lesson3/task6.py
@@ -7,11 +7,35 @@
 # начинаться с заглавной буквы. Необходимо использовать написанную ранее
 # функцию int_func().
 
+def int_func(word, result):
+    latin_flag = 0  # 1, если слово состоит только из латинских букв в нижнем регистре
+    if word.isalpha() and word.islower():
+        j = 0
+        while j < len(word):
+            if latin.find(f'{word[j]}', 0, len(latin)) != -1:
+                latin_flag = 1
+                j += 1
+            else:
+                j += 1
+                latin_flag = 0
+                break
+        if latin_flag == 1:
+            result = result + word.title() + ' '
+    return result
+
+result = ''
+latin = 'abcdifghijklmnopqrstuvwxyz'
 str = input('\nВведите одно или несколько слов в нижнем регистре'
-            'латиницей, отделяя их пробелами, и нажмите Enter: ')
+            ' латиницей, отделяя их пробелами, и нажмите Enter: ')
 list = str.split(' ')
 i = 0
-while i in list:
+while i < len(list):
     word = list[i]
-    if word.isalpha() and word.islower():
-        list.search
+    result = int_func(word, result)
+    i += 1
+
+if result == '':
+    print('Вы вводите что-то не то!! Попробуйте снова.')
+else:
+    print(f'\nВы ввели вот такую строку: "{str}".\nПрограмма выбрала все слова, написанные'
+          f'в нижнем регистре латинскими буквами, и записала их с большой буквы:\n "{result[0:len(result)-1]}"')
