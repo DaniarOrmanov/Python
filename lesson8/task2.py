0787815d93821ab9b43800df0c5659ab25d050df
Daniar <daniar.ormanov1989@yandex.ru>
2021-02-24 23:40:47 +0300

    lesson8

--- lesson8/task2.py
+++ lesson8/task2.py
@@ -3,3 +3,29 @@
 # вводимых пользователем. При вводе пользователем нуля в качестве
 # делителя программа должна корректно обработать эту ситуацию и не
 # завершиться с ошибкой.
+
+class Exc:
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def exc_0(self):
+        flag = 1
+        try:
+            num1 = float(self.num1)
+            num2 = float(self.num2)
+            result = num1 / num2
+        except ValueError:
+            print('Вы ввели не число!')
+        except ZeroDivisionError:
+            print('Делить на ноль нельзя!')
+        else:
+            print(num1 / num2)
+
+
+number1 = input('Введите первое число: ')
+number2 = input('Введите второе число: ')
+
+quot = Exc(number1, number2)
+quot.exc_0()
